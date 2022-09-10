3418007742a47284eb84ed83f6742bbb7e73c624
Stany1987 <imstany@gmail.com>
2022-09-10 21:48:28 +0300

    Final

--- src/main/java/com/example/timefx/HelloApplication.java
+++ src/main/java/com/example/timefx/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.timefx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,14 +16,22 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 350, 140);
+        stage.setResizable(false);
         stage.setTitle("Таймер выключения");
+        stage.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
         launch();
+
     }
+
 
 }
 
